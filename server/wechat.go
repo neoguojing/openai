98d8c3b0c7f4b3fc54a92ba9efdf21d8ca500be4
guoerjun <guoerjun@sensetime.com>
2023-06-29 16:34:39 +0800

    up

--- server/wechat.go
+++ server/wechat.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"sync"
 
 	"github.com/neoguojing/log"
@@ -65,22 +66,46 @@ func officeAccountHandler(c *gin.Context) {
 	// 传入request和responseWriter
 	officialAccountServer = officialAccount.GetServer(c.Request, c.Writer)
 	// 设置接收消息的处理方法
-	officialAccountServer.SetMessageHandler(func(msg *message.MixMessage) *message.Reply {
+	officialAccountServer.SetMessageHandler(func(msg *message.MixMessage) []message.Reply {
 		var aiText string
 		var err error
 		if msg.MsgType == message.MsgTypeText {
 			aiText, err = chat.Dialogue(models.Text, msg.Content, "", nil)
 			if err != nil {
 				log.Error(err.Error())
-				return &message.Reply{MsgType: message.MsgTypeText, MsgData: "ops"}
+				return []message.Reply{{MsgType: message.MsgTypeText, MsgData: "ops"}}
 			}
 		} else if msg.MsgType == message.MsgTypeVoice {
 
 		} else {
 
 		}
-		text := message.NewText(aiText)
-		return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
+
+		// 计算消息内容的长度
+		messageLength := len(aiText)
+
+		// 计算消息需要分成多少段
+		segmentCount := int(math.Ceil(float64(messageLength) / 2048.0))
+		replys := []message.Reply{}
+		// 分段发送消息
+		for i := 0; i < segmentCount; i++ {
+			// 计算当前段的起始位置和长度
+			start := i * 2048
+			length := 2048
+			if start+length > messageLength {
+				length = messageLength - start
+			}
+
+			// 截取当前段的消息内容
+			segment := aiText[start : start+length]
+			text := message.NewText(segment)
+			reply := message.Reply{MsgType: message.MsgTypeText, MsgData: text}
+			replys = append(replys, reply)
+
+		}
+
+		log.Infof("segmentCount:%v", segmentCount)
+		return replys
 	})
 
 	// 处理消息接收以及回复
